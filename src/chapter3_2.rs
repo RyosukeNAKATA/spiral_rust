0626c1e5df119ce615d846a2bdb099ceb04ec4b7
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-04-17 15:39:35 +0900

    rename file

--- src/chapter3_2.rs
+++ src/chapter3_2.rs
@@ -4,6 +4,24 @@ use std::io;
 fn main() {
     let n: usize = read();
     let mut a: Vec<i32> = read_vec();
+
+    for i in 0..n - 1 {
+        let mut v = a[i];
+        let mut j = i - 1;
+        while j >= 0 && a[j] > v {
+            a[j + 1] = a[j];
+            j -= 1;
+        }
+        a[j + 1] = v;
+
+        for k in 0..n {
+            if k != n - 1 {
+                print!("{} ", a[k]);
+            } else {
+                println!("{}", a[k]);
+            }
+        }
+    }
 }
 
 fn read<T: std::str::FromStr>() -> T {
